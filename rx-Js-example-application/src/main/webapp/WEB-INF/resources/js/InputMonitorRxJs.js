a1b85d571cbc34fa8f2308a0820d549428b999ed
Ayesh Almeida <ayeshs@hsenidmobile.com>
2017-04-07 17:17:34 +0530

    Update: GET Promise.

--- rx-Js-example-application/src/main/webapp/WEB-INF/resources/js/InputMonitorRxJs.js
+++ rx-Js-example-application/src/main/webapp/WEB-INF/resources/js/InputMonitorRxJs.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
     Rx.Observable.fromEvent(
 				document.getElementById("author-name"), 'keyup').
-			throttleTime(500).
+			throttleTime(10).
 			map((e) => e.target.value).
 			distinctUntilChanged().
 			switchMap(
-				(e) => $.get("http://localhost:8080/application?author="+e), 
-				(e) => $.get("http://localhost:8080/book?author="+e),
+				(e) => Rx.Observable.fromPromise($.get("http://localhost:8080/application?author="+e)),
+				(e) => Rx.Observable.fromPromise($.get("http://localhost:8080/book?author="+e)),
 				(applications, books) => applications.concat(books)
 			).
-			subscribe(e => console.log(typeof(e));
+			subscribe((e) => console.log(e.promise));
 
 });
