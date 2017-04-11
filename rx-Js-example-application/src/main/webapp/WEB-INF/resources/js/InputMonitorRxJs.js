33ffa4991450e62cb73d1c6e84571d4aa9217ada
Ayesh Almeida <ayeshs@hsenidmobile.com>
2017-04-11 15:24:56 +0530

    Update: RxJs Data, wip

--- rx-Js-example-application/src/main/webapp/WEB-INF/resources/js/InputMonitorRxJs.js
+++ rx-Js-example-application/src/main/webapp/WEB-INF/resources/js/InputMonitorRxJs.js
@@ -4,11 +4,11 @@ $(document).ready(function () {
 			throttleTime(10).
 			map((e) => e.target.value).
 			distinctUntilChanged().
-			switchMap(
-				(e) => Rx.Observable.fromPromise($.get("http://localhost:8080/application?author="+e)),
-				(e) => Rx.Observable.fromPromise($.get("http://localhost:8080/book?author="+e)),
-				(applications, books) => applications.concat(books)
-			).
-			subscribe((e) => console.log(e.promise));
-
+			switchMap(function(e) {
+				return Rx.Observable.forkJoin(
+					[$.get("http://localhost:8080/application?author="+e), 
+					$.get("http://localhost:8080/book?author="+e)]);
+			}).
+			reduce((applications, books) => applications.concat(books)).
+			subscribe((e) => console.log(e));
 });
